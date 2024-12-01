01819b6d133e75c5841f522b7eac7312a5be8466
Gon <o.gonzalo@hotmail.com>
2024-12-01 19:56:38 -0300

    remove unused sql plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![book::get_books_from_specified_dir])
