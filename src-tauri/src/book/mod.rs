9ac603139560890c9271d5fe7f8f7ee65dadf108
Gon <o.gonzalo@hotmail.com>
2024-12-02 20:40:04 -0300

    @backend add_retrieve_book_by_title func that filters by encoded title param

--- src-tauri/src/book/mod.rs
+++ src-tauri/src/book/mod.rs
@@ -1,10 +1,44 @@
-use std::{fs, io};
-
-use tauri::AppHandle;
+use std::{
+    fs,
+    io::{self},
+};
+use tauri::{AppHandle, Error};
 
 use crate::get_default_folder;
 
 pub mod epub;
+
+use percent_encoding::percent_decode_str;
+
+#[tauri::command]
+pub fn retrieve_book_by_title(
+    title: &str,
+    app_handle: AppHandle,
+) -> Result<epub::Book, tauri::Error> {
+    let books = get_books_from_existing_dir(app_handle)
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    let normalized_search = percent_decode_str(title)
+        .decode_utf8_lossy()
+        .replace('-', " ")
+        .to_lowercase();
+
+    let existing_book = books
+        .iter()
+        .find(|b| {
+            let decoded_title = percent_decode_str(&b.title)
+                .decode_utf8_lossy()
+                .to_lowercase();
+            let normalized_title = decoded_title.replace('-', " ");
+
+            print!("Matches? {:?}", normalized_title == normalized_search);
+            normalized_title == normalized_search
+        })
+        .ok_or_else(|| Error::WindowNotFound)?;
+
+    Ok(existing_book.clone())
+}
 
 #[tauri::command]
 pub fn retrieve_books(dir_path: &str) -> Result<Vec<epub::Book>, tauri::Error> {
