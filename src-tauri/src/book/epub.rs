816270f286207d3866fc974e4e42acc9b1a9791d
Gon <o.gonzalo@hotmail.com>
2024-12-02 20:39:38 -0300

    @backend: return raw_markup in BookMetadata prop

--- src-tauri/src/book/epub.rs
+++ src-tauri/src/book/epub.rs
@@ -1,24 +1,25 @@
 use epub::doc::EpubDoc;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BookMetadata {
     chapters: Option<i64>,
     raw_markup: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Book {
-    title: String,
-    author: String,
+    pub title: String,
+    pub author: String,
 
-    published_at: String,
-    metadata: BookMetadata,
+    pub published_at: String,
+    pub metadata: BookMetadata,
 }
 
 pub fn get_book_from_path(file_path: &str) -> Book {
     let epub_file_result = EpubDoc::new(file_path);
-    let epub_file = epub_file_result.unwrap();
+
+    let mut epub_file = epub_file_result.unwrap();
     let title = epub_file
         .mdata("title")
         .unwrap_or_else(|| "Unknown title".to_string());
@@ -28,6 +29,13 @@ pub fn get_book_from_path(file_path: &str) -> Book {
     let published_at = epub_file
         .mdata("date")
         .unwrap_or_else(|| "Unknown published at".to_string());
+    let mut raw_content = String::new();
+    let chapters = epub_file.spine.clone();
+    for (_, item) in chapters.iter().enumerate() {
+        if let Some((content, _)) = epub_file.get_resource(&item) {
+            raw_content.push_str(&String::from_utf8_lossy(&content));
+        }
+    }
 
     Book {
         title,
@@ -37,7 +45,7 @@ pub fn get_book_from_path(file_path: &str) -> Book {
         metadata: {
             BookMetadata {
                 chapters: None,
-                raw_markup: None,
+                raw_markup: Some(raw_content),
             }
         },
     }
