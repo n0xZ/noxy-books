c011fbd34cc4f49b4fa36e10594fbc61e499cbe3
Gon <o.gonzalo@hotmail.com>
2024-12-02 16:28:08 -0300

    @backend: add define_base_book_folder  func, that creates a settings.json file, containing book dir path, that must be consumed by the following book fn.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,24 @@
+use std::env;
+
+use tauri::Manager;
+use tauri_plugin_store::StoreExt;
 mod book;
 
+#[tauri::command]
+fn define_base_book_folder(
+    dir_path: &str,
+    app_handle: tauri::AppHandle,
+) -> Result<(), tauri::Error> {
+    let app_result = app_handle.path().local_data_dir().unwrap();
+    let app_dir = app_result.join("settings.json");
+    let default_settings = serde_json::json!({ "default_folder": dir_path });
+    std::fs::write(app_dir, default_settings.to_string()).map_err(|e| {
+        println!("An error ocurred while writing file: {:?}", e);
+        e
+    })?;
+
+    Ok(())
+}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -7,7 +26,11 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![book::get_books_from_specified_dir])
+        .invoke_handler(tauri::generate_handler![
+            book::get_books_from_existing_dir,
+            define_base_book_folder,
+            get_default_folder
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
